f7d07517fbe3d2294ba9b50f41300102c3162c12
Sai Ravi Teja Gangavarapu <73626236+SaiRaviTejaG-gif@users.noreply.github.com>
2021-01-28 01:49:43 +0530

    Update Rockpaperscissors.py

--- Rock-Paper-Scissors-Game/Rockpaperscissors.py
+++ Rock-Paper-Scissors-Game/Rockpaperscissors.py
@@ -1,16 +1,17 @@
 import random
+
 tools = ["rock", "paper", "scissors"]
 
 while True:
-    bot_tool=tools[random.randint(0,2)]
+    bot_tool = tools[random.randint(0, 2)]
     player_tool = input("Rock or paper or scissor?: ").lower()
     if player_tool == bot_tool:
         print("Tie!")
     elif player_tool == tools[0]:
         if bot_tool == tools[1]:
             print("You lost!")
-    elif bot_tool == tools[2]:
-        print("You won!")
+        elif bot_tool == tools[2]:
+            print("You won!")
     elif player_tool == tools[1]:
         if bot_tool == tools[0]:
             print("You won!")
@@ -28,12 +29,13 @@ while True:
     'paper' or
     'scissors' or
     'quit'
-
     """)
     elif player_tool == "quit":
         break
-    elif player_tool != tools[0] and player_tool != tools[1] and player_tool != tools[2] and player_tool != "help" and player_tool != "quit":
+    elif player_tool != tools[0] and player_tool != tools[1] and player_tool != tools[2]\
+            and player_tool != "help" and player_tool != "quit":
         print("""
         ERROR->type 'Help' for the allowed commands.
-        
+
         """)
+
